34444359ded71707b12c53c6622e1527c972d8ae
sidseal <sidseal@me.com>
2020-06-25 18:28:14 -0400

    allsystemsgo

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,6 +1,6 @@
 // const Op = Sequelize.Op;
 const db = require("../models");
-module.exports = function (app) {
+module.exports = function(app) {
   // app.get("/", (req, res) => {
   //   db.Exercises.findAll({}).then(results => {
   //     // results are available to us inside the .then
@@ -16,23 +16,29 @@ module.exports = function (app) {
   // });
 
   app.get("/", (req, res) => {
-
     db.Exercises.findAll({}).then(results => {
       // Object
       // const obj = {
       //   days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
       //   exercises: results
       // };
-           
+      const days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
       const obj = {
-        days: ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"],
-        exercises: ["Push-Ups"]
+        days: []
+        // days: ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"],
+        // exercises: ["Push-Ups"]
       };
       console.log(obj);
 
       //render page
-      res.render("index", obj);
+      for (let i = 0; i < 7; i++) {
+        obj.days.push({
+          name: days[i],
+          workout: results[i].name
+        });
+      }
 
+      res.render("index", obj);
     });
   });
 };
